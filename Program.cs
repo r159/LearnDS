394c6dfe73f3db478e0e386401828b5edcdf5397
r159 <rahulnair159@gmail.com>
2018-01-11 13:07:51 +0530

    Stack Data Structure implementation in C#

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Stack Implementation");
+            Console.WriteLine("Enter Element to stack");
+            StackImplementation<object> stack = new StackImplementation<object>();
+            stack.push(1);
+            stack.push("one");
+            stack.display();
+            object poppedElement = stack.pop();
+            Console.WriteLine("Popped Element: : " + poppedElement);
+            poppedElement = stack.pop();
+            Console.WriteLine("Popped Element: : " + poppedElement);
+          
+            Console.ReadLine();
+
+
+        }
+    }
+
+    
+}
---
+++ StackImplementation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class StackImplementation<T> : IStack<T>
+    {
+        private T[] stackelemts;
+        private int currentIndex;
+        public StackImplementation()
+        {
+            stackelemts = new T[10];
+            currentIndex = 0;
+
+        }
+        public StackImplementation(int numofElements)
+        {
+            if (numofElements < 0)
+                throw new ArgumentOutOfRangeException("N");
+            else
+            {
+                stackelemts = new T[numofElements];
+                currentIndex = 0;
+            }
+        }
+
+        public void push(T element)
+        {
+            if (currentIndex + 1 > stackelemts.Length)
+            {
+                Console.WriteLine("Stack overflow");
+            }
+            else
+            {
+                stackelemts[currentIndex++] = element;
+            }
+        }
+        public T pop()
+        {
+            if (currentIndex == 0)
+                throw new InvalidOperationException("The Stack is Empty");
+
+            T element = stackelemts[--currentIndex];
+            stackelemts[currentIndex] = default(T);
+            return element;
+        }
+        public void display()
+        {
+            for(int i=currentIndex-1; i>=0;i--)
+            {
+                Console.WriteLine("Item {0}: {1}", (i), stackelemts[i]);
+                
+            }
+        }
+    }
+    interface IStack<T>
+    {
+        void push(T element);
+        T pop();
+        void display();
+    }
+}
