65ec5ac907cdca2c1fab940b1b4e9db886cf8206
r159 <rahulnair159@gmail.com>
2018-02-07 14:13:26 +0530

    Added Queues and LinkedList

--- Program.cs
+++ Program.cs
@@ -24,6 +24,37 @@ namespace Stack
             Console.WriteLine("Popped Element: : " + poppedElement);
           
             Console.ReadLine();
+             Console.WriteLine("Queue Implementation");
+            Console.WriteLine("Elements to queue");
+            QueueImplementation<object> queue = new QueueImplementation<object>(3);
+            queue.Enqueue(1);
+            queue.Enqueue("two");
+            queue.Enqueue(3);
+            queue.Enqueue("Four");
+            queue.display();
+            Console.ReadLine();
+            queue.Dequeue();
+            queue.Dequeue();
+            Console.WriteLine("After Dequeue");
+            queue.display();
+            Console.ReadLine();
+
+
+            Console.WriteLine("LinkedList Implementation");
+            LinkedListImplementation linklst = new LinkedListImplementation();
+            linklst.AppendToEnd(7);
+            Console.WriteLine("Linked List are:");
+            linklst.printList();
+            linklst.AppendToEnd("Eight");
+            linklst.AppendToEnd("Nine");
+            linklst.push(5);
+
+            Console.WriteLine("Linked List are:");
+            linklst.printList();
+
+            linklst.insertAfterNode(linklst.head, 6);
+            Console.WriteLine("Linked List are:");
+            linklst.printList();
 
 
         }
